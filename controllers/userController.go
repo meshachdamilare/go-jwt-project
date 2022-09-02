17d24a79fbc8e701ccf040688748bb49152df428
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-02 14:29:43 +0100

    declared all the functions needed

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,20 @@
 package controllers
+
+import (
+	"github.com/Christomesh/go-jwt-project/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+func HashPassword()
+
+func VerifyPassword()
+
+func Login()
+
+func Signup()
+
+func GetAllUsers()
+
+func GetUser()
