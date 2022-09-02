1da18fe44e43671ec908f60a98323715ea87451e
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-02 13:20:38 +0100

    added users routes

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,7 +1,13 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	controller "github.com/Christomesh/go-jwt-project/controllers"
+	"github.com/Christomesh/go-jwt-project/middleware"
+	"github.com/gin-gonic/gin"
+)
 
-func UserRoutes(incomingRoute *gin.Engine) {
-
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authentication())
+	incomingRoutes.GET("users/", controller.GetAllUsers())
+	incomingRoutes.GET("users/:user_id", controller.GetUser())
 }
