5926dc5fd08932222eb2ba2f98f387019a0566ab
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-02 12:42:45 +0100

    updated main.go file

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/Christomesh/go-jwt-project/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("api/v1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Acsess granted for api v1"})
+	})
+
+	router.GET("api/v2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Acsess granted for api v2"})
+	})
+
+	router.Run(":" + port)
 }
