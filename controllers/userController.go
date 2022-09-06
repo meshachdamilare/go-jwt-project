0174c68ff715d249b86c5996d937bc3c9f90f5b3
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-06 04:09:23 +0100

    worked on getuser route

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,7 +1,15 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/Christomesh/go-jwt-project/database"
+	helper "github.com/Christomesh/go-jwt-project/helpers"
+	"github.com/Christomesh/go-jwt-project/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -13,8 +21,28 @@ func VerifyPassword()
 
 func Login()
 
-func Signup()
+func Signup() gin.HandlerFunc {
+
+}
 
 func GetAllUsers()
 
-func GetUser()
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("userId")
+
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
+}
