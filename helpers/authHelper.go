4dbd06b16a0d4a7af374e540042ef4a4acd8cf1a
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-06 04:10:12 +0100

    helper

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,1 +1,28 @@
 package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	if userType != role {
+		err = errors.New("Unauthorized to access this routes")
+		return err
+	}
+	return err
+}
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorized to access this routes")
+		return err
+	}
+	err = CheckUserType(c, userType)
+	return err
+}
