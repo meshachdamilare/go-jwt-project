db6d3f56d0166dc0b524eff112355f5efca5f5e3
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-07 13:10:38 +0100

    added authMiddleware

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,1 +1,30 @@
 package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	helper "github.com/Christomesh/go-jwt-project/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"err": fmt.Sprintf("No authorization header provided")})
+			c.Abort()
+			return
+		}
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("user_id", claims.Uid)
+		c.Next()
+
+	}
+}
