a0eeda29b99015721f8876c4186a2bf805cf63f2
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-08 05:45:21 +0100

    fixed

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -8,6 +8,6 @@ import (
 
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authentication())
-	incomingRoutes.GET("users/", controller.GetAllUsers())
-	incomingRoutes.GET("users/:user_id", controller.GetUser())
+	incomingRoutes.GET("/users", controller.GetAllUsers())
+	incomingRoutes.GET("/users/:userId", controller.GetUser())
 }
