f5734f72ffa2e6b423b9b0d468f1624cd64e139f
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-09-08 05:45:40 +0100

    added login model

--- models/userModel.go
+++ models/userModel.go
@@ -6,18 +6,23 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type User struct {
+type UserModel struct {
 	ID         primitive.ObjectID `bson:"_id"`
-	First_name *string            `json:"first_name" validate:"required,min=2,max=100"`
-	Last_name  *string            `json:"last_name" validate:"required,min=2,max=100"`
-	Password   *string            `json:"password" validate:"required,min=6"`
-	Email      *string            `json:"email" validate:"email,required"`
-	Phone      *string            `json:"phone" validate:"required"`
-	Token      *string            `json:"token"`
+	First_name string             `json:"first_name" validate:"required,min=2,max=100"`
+	Last_name  string             `json:"last_name" validate:"required,min=2,max=100"`
+	Password   string             `json:"password" validate:"required,min=6"`
+	Email      string             `json:"email" validate:"email,required"`
+	Phone      string             `json:"phone" validate:"required"`
+	Token      string             `json:"token"`
 	// create an enum of choice of admin or user like in javascript
-	User_type     *string   `json:"user_type" validate:"required,eq=AMIN|eq=USER"`
-	Refresh_token *string   `json:"refresh_token"`
+	User_type     string    `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
+	Refresh_token string    `json:"refresh_token"`
 	Created_at    time.Time `json:"created_at"`
 	Updated_at    time.Time `json:"updated_at"`
 	User_id       string    `json:"user_id"`
+}
+
+type LoginModel struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
